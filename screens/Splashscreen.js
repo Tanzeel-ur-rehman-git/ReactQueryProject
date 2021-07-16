f735ba791b38eb440d969867831ed53222269887
Tanzeel-ur-rehman-git <mr.tanzeel@gmail.com>
2021-07-16 22:46:21 +0500

    update

---
+++ screens/Splashscreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class Splashscreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> Splashscreen </Text>
+      </View>
+    );
+  }
+}
+
+export default Splashscreen;
